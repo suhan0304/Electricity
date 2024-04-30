695a2c56a4d1417253f2202c5105e6a6027b71d0
ZerrY <suhan0304@gmail.com>
2024-04-30 20:51:29 +0900

    Update BuildManager
    - BuildManager get blockToBuild GameObject Prefab
    - Singleton pattern, any scripts can use build

---
+++ Electricity/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance; // Singleton Pattern
+
+    private void Awake() // For singleton 
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardBlockPrefab;
+
+    private void Start()
+    {
+        blockToBuild = standardBlockPrefab; //For Test
+    }
+
+    private GameObject blockToBuild; // blockToBuild GameObject
+
+    public GameObject GetBlockToBuild() // return blockToBuild;
+    {
+        return blockToBuild;
+    }
+}
