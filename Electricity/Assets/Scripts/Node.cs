8681fcf2067ff63ba3de9dd3c95974a9dd1acc20
ZerrY <suhan0304@gmail.com>
2024-04-30 20:42:33 +0900

    Update hoverColor Node
    - when mouse on node, Change color to hoverColor
    - when mouse leave node, Change color to startColor

--- Electricity/Assets/Scripts/Node.cs
+++ Electricity/Assets/Scripts/Node.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public Color hoverColor; // color changes when mouse is hovered over
+
+    private Renderer rend; // renderer component
+    private Color startColor;  // start color
+
+    private void Start()
     {
-        
+        rend = GetComponent<Renderer>(); // call renderer component
+        startColor = rend.material.color; // remember start color
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseEnter() // When the mouse passes or enters an object collider
     {
-        
+        rend.material.color = hoverColor; // change color to hoverColor
     }
+
+    private void OnMouseExit() //
+    {
+        rend.material.color = startColor; // return color to startColor
+    }
+
 }
