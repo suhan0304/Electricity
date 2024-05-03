77e99c09e945a0e4435a86a400889dae2a4742f7
ZerrY <suhan0304@gmail.com>
2024-05-03 22:10:06 +0900

    Update Block.cs

--- Electricity/Assets/Scripts/Block.cs
+++ Electricity/Assets/Scripts/Block.cs
@@ -1,8 +1,22 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    CinemachineFreeLook freelook;
+    public float zoomSpeed = 1000.0f;
 
+    private void Update()
+    {
+        if (Input.GetKey("q")) // Press "q" Key = Zoom in
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("e")) // Pree "e" Key = Zoom out
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+    }
 }
