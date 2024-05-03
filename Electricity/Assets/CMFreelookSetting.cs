dfbd6ff477edc9ff02417cb265f5725fa0508de8
ZerrY <suhan0304@gmail.com>
2024-05-03 22:18:53 +0900

    Update Zoom IN/OUT Function
    - Update Zoom In/Out when press "q","e" key

--- Electricity/Assets/CMFreelookSetting.cs
+++ Electricity/Assets/CMFreelookSetting.cs
@@ -1,18 +1,29 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CMFreelookSetting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    CinemachineFreeLook freelook;
+    public float zoomSpeed = 2.5f;
+
+    private void Start()
     {
-        
+        freelook = GetComponent<CinemachineFreeLook>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey("q")) // Press "q" Key = Zoom in
+        {
+            if(freelook.m_Lens.FieldOfView >= 5)
+                freelook.m_Lens.FieldOfView -= zoomSpeed * Time.deltaTime; //Zoom In
+        }
+        if (Input.GetKey("e")) // Pree "e" Key = Zoom out
+        {
+            if (freelook.m_Lens.FieldOfView <= 20)
+                freelook.m_Lens.FieldOfView += zoomSpeed * Time.deltaTime; //Zoom Out
+        }
     }
 }
--- Electricity/Assets/Scripts/Block.cs
+++ Electricity/Assets/Scripts/Block.cs
@@ -5,18 +5,8 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    CinemachineFreeLook freelook;
-    public float zoomSpeed = 1000.0f;
-
-    private void Update()
+    private void Start()
     {
-        if (Input.GetKey("q")) // Press "q" Key = Zoom in
-        {
-            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
-        }
-        if (Input.GetKey("e")) // Pree "e" Key = Zoom out
-        {
-            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
-        }
+        
     }
 }
