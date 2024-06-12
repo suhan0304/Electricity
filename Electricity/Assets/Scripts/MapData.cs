447989ac8b4f605edfefe471767d8cb592dc24e3
ZerrY <suhan0304@gmail.com>
2024-06-12 10:37:18 +0900

    Update MapData.cs

--- Electricity/Assets/Scripts/MapData.cs
+++ Electricity/Assets/Scripts/MapData.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class MapData : MonoBehaviour
+[System.Serializable]
+public class MapData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 startNode; // StartNode의 좌표
+    public Vector3 endNode; // EndNode의 좌표
+    public List<Vector3> nodePositions; // 모든 노드들의 좌표
+    public Dictionary<int, int> blockInventory; // 블록 타입별 보유 개수 (예: {1: 2, 3: 4})
 
-    // Update is called once per frame
-    void Update()
+    public MapData(Vector3 start, Vector3 end)
     {
-        
+        startNode = start;
+        endNode = end;
+        nodePositions = new List<Vector3>();
+        blockInventory = new Dictionary<int, int>();
     }
 }
